f4f65ce04ad8750cbea1a0c09658ac9bba2d0075
Marcel Dancak <dancakm@gmail.com>
2022-08-21 18:43:48 +0200

    updated layers tree props after changes in upstream

--- clients/gisquick-web/src/components/content-panel/ContentPanel.vue
+++ clients/gisquick-web/src/components/content-panel/ContentPanel.vue
@@ -13,7 +13,7 @@
         <layers-tree
           class="light"
           :attribute-table-disabled="attributeTableDisabled"
-          :layers="project.overlays.tree"
+          :layers="project.overlays"
           :expanded.sync="expandedOverlays"
         />
       </scroll-area>
